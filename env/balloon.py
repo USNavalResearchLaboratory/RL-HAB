55efdf8343dbec64aa19645190c31390fb61d938
schuler <tristan.schuler@nrl.navy.mil>
2024-10-25 10:15:17 -0400

    Forgot to add the env directory

---
+++ env/balloon.py
@@ -0,0 +1,116 @@
+
+#from era5 import config_earth
+import pandas as pd
+import enum
+from env.config.env_config import env_params
+import numpy as np
+import math
+
+class BalloonState(object):
+    '''Balloon State object for for updating and referencing during a simulation
+
+    Attributes:
+        x: Balloons relative x position in m converted from lat/lng to cartesian using a spherical coordinate
+        transformation (see latlon_to_meters_spherical in utils)
+        y: Balloons relative x position in m converted from lat/lng to cartesian using a spherical coordinate
+        transformation (see latlon_to_meters_spherical in utils)
+
+        altitude: Balloon's absolute (m)
+
+        x_vel: Balloon's current velocity in m/s
+        y_vel: Balloon's current velocity in m/s
+        z_vel: Balloon's current ascent/descent velocity in m/s
+
+        lat: The current latitude of balloon  (what numbering style?)
+        lon: The current longitude of balloon (what numbering style?)
+
+        distance: Current Distance to Station (XY plane) in m
+        rel_bearing:  The relative bearing of the direction of motion the balloon is traveling to in reference to the station
+
+        atm_pressure: The atmospheric pressure around the balloon at the current altitude
+    '''
+
+    def __init__(self, x=None, y=None, altitude=None, x_vel=None, y_vel=None, z_vel=None,
+                 distance=None, lat=None, lon=None, rel_bearing=None, pressure=None):
+        #Trying to initialize to 0 to correct occasional error sith synth winds.
+        self.x = x
+        self.y = y
+        self.altitude = altitude
+
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.z_vel = z_vel
+
+        self.lat = lat
+        self.lon = lon
+
+        self.distance= distance
+        self.rel_bearing = rel_bearing
+
+        self.atm_pressure = 0  #Figure this out later
+
+        self.last_action= 0
+
+        self.rel_wind_column= None #this might be the isssue
+
+        #For later
+        power= 0
+
+    def __str__(self):
+        return (f"BalloonState(x={self.x}, y={self.y}, altitude={self.altitude},\n "
+                f"dist={self.distance}, rel_bearing={self.rel_bearing},\n "
+                f"x_vel={self.x_vel}, y_vel={self.y_vel}, z_vel={self.z_vel},\n "
+                f"lat={self.lat}, lon={self.lon}),\n"
+                f"last_action={self.last_action}, rel_wind_column={self.rel_wind_column})")
+
+    def update(self, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+
+
+class AltitudeControlCommand(enum.IntEnum):
+    """Altitude Control Command"""
+    DOWN = 0
+    STAY = 1
+    UP = 2
+
+class SimulatorState(object):
+    '''
+    A Simulator Class for keeping track of overall simulator state variables
+
+    '''
+    def __init__(self,Balloon, timestamp):
+
+        self.Balloon = Balloon
+
+        self.timestamp = timestamp
+        self.dt = env_params['dt']
+        self.total_steps = 0
+
+        self.episode_length = env_params['episode_length']
+
+        self.trajectory = []
+        self.time_history = []
+
+    def step(self,Balloon):
+        '''
+        Steps the simulator global variables and updates accordingly.  This should match the step rate in gym environments
+
+        :param Balloon:
+        :return:
+        '''
+
+        self.timestamp = self.timestamp + pd.Timedelta(hours=(1 / 3600 * self.dt))
+
+        self.trajectory.append((Balloon.x , Balloon.y, Balloon.altitude))
+        self.time_history.append(self.timestamp)
+
+        if self.total_steps > self.episode_length - 1:
+            done = True
+        else:
+            done = False
+
+        self.total_steps += 1
+
+        return done
+
---
+++ env/forecast_processing/ForecastClassifier.py
@@ -0,0 +1,115 @@
+import numpy as np
+from env.forecast_processing.forecast import Forecast, Forecast_Subset
+import windrose
+import pandas as pd
+from env.forecast_processing.forecast_visualizer import ForecastVisualizer
+import matplotlib.pyplot as plt
+from env.config.env_config import env_params
+
+
+class ForecastClassifier:
+    def __init__(self):
+        pass
+
+    def determine_opposing_winds(self, wd, levels, n_sectors):
+
+        dir_edges, var_bins, table = windrose.windrose.histogram(wd, levels, bins=levels, nsector=n_sectors)
+
+        #Determine the sectors (directions) that contain non zero values (altitude levels that have wind)
+        df = pd.DataFrame(table)
+
+
+        altitude_lookup_idxs = df.apply(np.flatnonzero, axis=0) # altitude can be pressure or height, depending on by_pressure variable
+
+        opposing_wind_levels = np.array([])
+        opposing_wind_directions = np.array([])
+
+        # Determine the sectors that have opposing winds by checking the current index and the complimentary index at n_sectors/2.
+        # Also determine the altitudes contains in the opposing wind pairs for calculating probabilities later.
+        for i in range (0,int(n_sectors/2)):
+            # check if opposing sectors in the histogram tables have values greater than 0
+            # (therefore, there are winds in that sectors)
+            if np.sum(table, axis=0)[i] != 0 and np.sum(table, axis=0)[i+int(n_sectors/2)] != 0:
+                for idx in altitude_lookup_idxs[i]:
+                    opposing_wind_levels = np.append(opposing_wind_levels, var_bins[idx])
+                    opposing_wind_directions = np.append(opposing_wind_directions, i)
+                for idx in altitude_lookup_idxs[i+int(n_sectors/2)]:
+                    #print(var_bins[idx])
+                    opposing_wind_levels = np.append(opposing_wind_levels, var_bins[idx])
+                    opposing_wind_directions = np.append(opposing_wind_directions, i+int(n_sectors/2))
+
+        # sort the opposing wind altitudes and direction idxs (format later) in ascending order and remove duplicates
+        opposing_wind_levels = np.sort(np.unique(opposing_wind_levels))
+        opposing_wind_directions = np.sort(np.unique(opposing_wind_directions))
+
+        return opposing_wind_directions, opposing_wind_levels
+
+
+    def determine_OW_Rate(self, forecast_subset):
+
+        #Assuming 24 hour subset window right now
+
+        start_time = forecast_subset.start_time
+        timestamp = start_time
+        scores = []
+
+        intervals = 4
+
+        n_sectors = 8
+
+        for i in range (0,intervals):
+
+            z, u, v = forecast_subset.np_lookup(forecast_subset.lat_central, forecast_subset.lon_central,
+                                                timestamp)
+
+            bearing, speed = forecast_subset.windVectorToBearing(u, v)
+            bearing = bearing % (2 * np.pi)
+            bearing = np.degrees(bearing)
+
+            levels = forecast_subset.ds.level.values
+            opposing_wind_directions, opposing_wind_levels = self.determine_opposing_winds(bearing, levels, n_sectors)
+
+            scores.append(len(opposing_wind_levels))
+
+            timestamp = timestamp + np.timedelta64(6, "h")
+
+
+        max_score = (forecast_subset.level_dim*intervals)
+
+        score = np.sum(scores)/max_score
+
+        return scores, score
+
+
+if __name__ == '__main__':
+    FORECAST_SYNTH = Forecast(env_params['synth_netcdf'], forecast_type="SYNTH")
+    # Get month associated with Synth
+    month = pd.to_datetime(FORECAST_SYNTH.TIME_MIN).month
+    # Then process ERA5 to span the same timespan as a monthly Synthwinds File
+    FORECAST_ERA5 = Forecast(env_params['era_netcdf'], forecast_type="ERA5", month=month)
+
+
+    forecast_subset = Forecast_Subset(FORECAST_SYNTH) #Choose FORECAST_SYNTH or FORECAST_ERA5 here
+    forecast_subset.randomize_coord()
+    print("random_coord", forecast_subset.lat_central, forecast_subset.lon_central, forecast_subset.start_time)
+    forecast_subset.subset_forecast()
+
+    ForecastClassifier = ForecastClassifier()
+    scores, score = ForecastClassifier.determine_OW_Rate(forecast_subset)
+
+    print(scores,score)
+
+    timestamp = forecast_subset.start_time
+    skip = 2
+
+    Forecast_visualizer = ForecastVisualizer(forecast_subset)
+    Forecast_visualizer.generate_flow_array(timestamp=timestamp)
+
+    # Initialize Figure
+    fig = plt.figure(figsize=(15, 10))
+    ax1 = fig.add_subplot(111, projection='custom3dquiver')
+
+    fig.add_axes(ax1)
+
+    Forecast_visualizer.visualize_3d_planar_flow(ax1, skip)
+    plt.show()
