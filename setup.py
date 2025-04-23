61131d0c64d9a1c653268b6cc066157adeb616d8
schuler(aeolus) <tristan.k.schuler.civ@us.navy.mil>
2025-04-23 11:49:51 -0400

    Repo name update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 # pip3 install -e .
 
 setup(
-    name='FLOW2D',
-    version='1.0.0',
+    name='RLHAB',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[],
 )
